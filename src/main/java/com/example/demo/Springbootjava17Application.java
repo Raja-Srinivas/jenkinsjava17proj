990a7393624590fe1f107f32fb592d839f92804b
Raja-Srinivas <94114452+Raja-Srinivas@users.noreply.github.com>
2025-02-04 17:36:01 +0530

    java commits

---
+++ src/main/java/com/example/demo/Springbootjava17Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootjava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootjava17Application.class, args);
+	}
+
+}
