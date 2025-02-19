5e40c77a2b250d8a19d8bb19f20907c5d1bbd27f
Raja-Srinivas <94114452+Raja-Srinivas@users.noreply.github.com>
2025-02-19 17:37:19 +0530

    Update TestController.java

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
